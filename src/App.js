d7e56aa65cdf1395f09200cea4a2897b1c55c1f1
Georgio Augusto Ardigo Junior <gardigo2001@gmail.com>
2023-06-14 18:04:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './build.png';
+import './App.css';
+import Navigation from './Components/Navigation';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
+
+const App = () => (
+  <div className='App'>
+    <Navigation />
+    <Main />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/Components/Navigation/index.js
@@ -0,0 +1,37 @@
+import './styles.css';
+import { NavLink } from 'react-router-dom';
+import image from '../../images-svg/svgs/3-lines.svg'
+import { useState } from 'react';
+
+
+
+const NavLinks = () => (
+  <ul class='Navbar-links'>
+    <li class='Navbar-link'><NavLink to='/'>Início</NavLink></li>
+    <li class='Navbar-link'><NavLink to='/sign-in'>Login</NavLink></li>
+    <li class='Navbar-link'><NavLink to='/sign-up'>Cadastro</NavLink></li>
+  </ul>
+);
+
+const Navigation = () => {
+  const [isMenuOpen, toggleMenu] = useState(false);
+
+  return (
+    <>
+      <nav className='Navbar'>
+        <div class='Navbar-logo'>Hotel Ardigo</div>
+        <img class='three-lines' src={image} onClick={(prev) => {
+          console.log(isMenuOpen);
+          toggleMenu(!isMenuOpen);
+        }}/>
+        <NavLinks />
+      </nav>
+      <div className={`navbar-links-mobile-container ${isMenuOpen ? 'show' : ''}`}>
+        <NavLinks />
+      </div>
+      
+    </>
+  )
+};
+
+export default Navigation
